16c04dafa627e183613a610fd780084717e9473a
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-10-05 16:39:30 +0000

    added relationship for device and employee. A device belongs to employee, and an employee has many devices

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
 class Employee < ApplicationRecord
   has_one_attached :profile_picture
-
+  has_many :devices
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_153521) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_163534) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_153521) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "employee_id"
+    t.index ["employee_id"], name: "index_devices_on_employee_id"
   end
 
   create_table "employees", force: :cascade do |t|
@@ -63,4 +65,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_153521) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "devices", "employees"
 end
