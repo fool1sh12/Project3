436cc4a586ceeac8f54c9bed6c714ce575e0f2aa
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-10-17 00:40:22 +0000

    Added device to category relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :categories, through: :devices
+  has_many :devices
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,4 @@
 class Device < ApplicationRecord
   belongs_to :employee
+  belongs_to :category
 end
