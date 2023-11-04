ce1eb0dda8d3cbd95809f36038517b1f203f7cbe
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-11-04 19:41:20 +0000

    added category id variable and connected it to second form input

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
-  
-    @devices = Device.where(:name => @query)
+    @category_id = params[:category_id]
+    
+    if @category_id.present? 
+      @category = Category.find(@category_id)
+    end
+
+    @devices = Device.where("name like '%#{@query}%'").where(category_id: @category_id)
   end
 end
