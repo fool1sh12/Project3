4d37ebd3ac53ed0e80f1854f1d5b5dbce0723d87
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-11-04 20:41:30 +0000

    added an and condition to filter results by both category and manufacturer

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,8 +8,15 @@ class SearchController < ApplicationController
 
     if @category_id.present? 
       @devices = @devices.where(category_id: @category_id)
-    elsif @manufacturer_id.present?
+    end
+
+    if @manufacturer_id.present?
       @devices = @devices.where(manufacturer_id: @manufacturer_id)
+    end
+
+    if @category_id.present? && @manufacturer_id.present?
+      @devices_with_cat = @devices.where(category_id: @category_id)
+      @devices = @devices_with_cat.where(manufacturer_id: @manufacturer_id)
     end
   end
 end
